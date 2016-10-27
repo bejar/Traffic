1c17c8c9326ac9c049549055e0b7c4626f0a7be0
javier.bejar <bejar@cs.upc.edu>
2016-10-27 14:45:55 +0200

    Initial commit

---
+++ Cameras.py
@@ -0,0 +1,47 @@
+"""
+.. module:: Cameras
+
+Cameras
+*************
+
+:Description: Cameras
+
+    
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 27/10/2016 8:34 
+
+"""
+
+__author__ = 'bejar'
+
+
+Cameras =[
+    'AragoPgGracia',
+    'BalmesGranVia',
+    'BalmesMitre',
+    'DiagonalCiutatdeGranada',
+    'DiagonalMCristina',
+    'MeridianaFelipII',
+    'MeridianaRioJaneiro',
+    'PlAntonioLopez',
+    'PlCatalunya',
+    'PlEspanya',
+    'PlMolina',
+    'PlPaissosCatalans',
+    'PlPauVila',
+    'PlUrquinaona',
+    'RondaLitoralBadajoz',
+    'RondadeDaltCrtaEsplugues',
+    'RondadeDaltMeridiana',
+    'RondadeDaltSantGervasi',
+    'RondadeDaltVelodrom',
+    'RondaLitoralBonPastor',
+    'RondaLitoralMollFusta',
+    'RondaLitoralZonaFranca',
+    'TunelRovira'
+]
---
+++ GetImages.py
@@ -0,0 +1,47 @@
+"""
+.. module:: GetImages
+
+GetImages
+*************
+
+:Description: GetImages
+
+    
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 27/10/2016 8:23 
+
+"""
+
+
+import requests
+from Cameras import Cameras
+import time
+
+__author__ = 'bejar'
+
+
+
+path = '/home/bejar/storage/Data/Traffic/'
+
+while True:
+    print('%s Retrieving Cameras' % time.strftime('%H:%M %d-%m-%Y',time.localtime()))
+    print(time.ctime(int(time.time())-600))
+    rtime = str((int(time.time())-600)*1000)
+    ctime = time.strftime('%Y%m%d%H%M', time.localtime())
+
+    for cam in Cameras:
+        #print('Retrieving %s ...' % cam)
+
+        img_data = requests.get('http://www.bcn.cat/transit/imatges/%s.gif?a=1&time=%s' % (cam,rtime)).content
+
+
+        with open(path+'%s-%s.gif' % (ctime, cam), 'wb') as handler:
+            handler.write(img_data)
+        time.sleep(10)
+
+    time.sleep(15 * 60)
