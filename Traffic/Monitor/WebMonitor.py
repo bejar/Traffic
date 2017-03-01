cf07d68b6f50b8ae207c942a597f8bb9785aa86c
bejar <bejar@lsi.upc.edu>
2017-03-01 13:14:57 +0100

    minor fix

--- Traffic/Monitor/WebMonitor.py
+++ Traffic/Monitor/WebMonitor.py
@@ -348,14 +348,11 @@ def stop():
     """
     payload = request.form['stop']
 
-    print payload
     client = MongoClient(mongoconnection.server)
     db = client[mongoconnection.db]
     db.authenticate(mongoconnection.user, password=mongoconnection.passwd)
     col = db[mongoconnection.col]
     vals = col.find_one({'_id': int(payload)}, {'host':1, 'stop':1})
-    print vals
-    print col.update({'_id':int(payload)}, {'$set': {'stop': True}})
 
     head = """
     <!DOCTYPE html>
