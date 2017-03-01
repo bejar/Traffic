635cf5412a948b3c93b751ffc174472204915e10
bejar <bejar@lsi.upc.edu>
2017-03-01 13:02:29 +0100

    minor fix

--- Traffic/Monitor/WebMonitor.py
+++ Traffic/Monitor/WebMonitor.py
@@ -346,7 +346,7 @@ def stop():
 
     :return:
     """
-    payload = request.form['model']
+    payload = request.form['stop']
 
     client = MongoClient(mongoconnection.server)
     db = client[mongoconnection.db]
